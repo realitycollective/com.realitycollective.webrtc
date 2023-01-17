7820999d680d5bf760dd1f7db861008f79fcb06c
Rony Portelli <rony@xrealitylabs.com>
2023-01-17 19:26:14 +0000

    Initial boilerplate code

---
+++ Runtime/Interfaces/IWebRTCService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using RealityCollective.ServiceFramework.Interfaces;
+
+namespace RealityToolkit.WebRTC
+{
+    public interface IWebRTCService : IService
+    {
+        /// <summary>
+        /// Event for when we receive data
+        /// </summary>
+        Action<string> OnDataReceived { get; }
+
+        /// <summary>
+        /// Create a connection with a remote peer
+        /// </summary>
+        /// <param name="localPeerId">Our local id for the peer connection</param>
+        /// <param name="remotePeerId">Id of the remote peer we want to connect to</param>
+        /// <param name="startConnection">Auto initialize and start the connection</param>
+        /// <returns></returns>
+        Task AddConnection(int localPeerId, int remotePeerId, bool startConnection);
+
+        /// <summary>
+        /// Remove a connection with a specific peer
+        /// </summary>
+        /// <param name="remotePeerId"></param>
+        void RemoveConnection(int remotePeerId);
+
+        /// <summary>
+        /// Remove all active connections
+        /// </summary>
+        void RemoveAllConnections();
+
+        /// <summary>
+        /// Send data across a webrtc connection
+        /// </summary>
+        /// <param name="remotePeerId">the id of the remote peer we want to send data to</param>
+        /// <param name="data">Data we want to send across the a webrtc connection</param>
+        /// <typeparam name="T">Type of data we want to send</typeparam>
+        void SendData<T>(int remotePeerId, T data);
+    }
+}
+
---
+++ Runtime/Services/WebRTCService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using RealityCollective.ServiceFramework.Services;
+
+namespace RealityToolkit.WebRTC
+{
+    public class WebRTCService : BaseServiceWithConstructor, IWebRTCService
+    {
+        
+    }
+}
+
