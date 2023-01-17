93fb4c790f79740c535c9826aa43b0ad9a041f61
Rony Portelli <rony@xrealitylabs.com>
2023-01-17 19:26:55 +0000

    Added the boilerplate code to the service

--- Runtime/Services/WebRTCService.cs
+++ Runtime/Services/WebRTCService.cs
@@ -1,11 +1,31 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using RealityCollective.ServiceFramework.Services;
 
 namespace RealityToolkit.WebRTC
 {
     public class WebRTCService : BaseServiceWithConstructor, IWebRTCService
     {
-        
+        public Action<string> OnDataReceived { get; }
+        public Task AddConnection(int localPeerId, int remotePeerId, bool startConnection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveConnection(int remotePeerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAllConnections()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendData<T>(int remotePeerId, T data)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
 
